7fb76e56b20c0dbc1d5a216fc39b55e1f8dace50
Jonica94 <springer.164@osu.edu>
2017-02-05 16:02:38 -0500

    Exercise 4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,16 +43,26 @@ public class LifeController : Controller
         return Ok("This is the quote of the LifeController");
     }
 }
-
+[Route ("customer")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
     }
-    [Route("customercare/claims")]
+    [Route("claims")]
     public IActionResult Quote()
     {
         return Ok("This is the quote of the CustomerCareController");
+    }
+    [Route ("fileclaim")]
+    public IActionResult NewClaim()
+    {
+        return Ok("This is the New claim of the CustomerCareController");
+    }
+    [Route ("claimstatus")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("This is the Claim History of the CustomerCareController");
     }
 }
