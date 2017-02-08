4b58a21cd8c284726b2ee5670201d28bfd968674
Jonica94 <springer.164@osu.edu>
2017-02-08 18:29:01 -0500

    Views

--- Controllers/HomesController.cs
+++ Controllers/HomesController.cs
@@ -3,7 +3,8 @@ public class HomesController : Controller
 {
     public IActionResult Index()
     {
-        return Ok("This is the index of the HomesController");
+        //return Ok("This is the index of the HomesController");
+        return View();
     }
     public IActionResult Quote()
     {
